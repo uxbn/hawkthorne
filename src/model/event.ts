67b09ab1dfd1e229b8541fffa632c58edb491c2b
Chris <chris@huskysoft.io>
2021-01-26 14:23:56 -0800

    Adds discord models and updates dependencies.

--- src/model/event.ts
+++
@@ -1,8 +0,0 @@
-import { UserRegistration } from './user_registration';
-
-export interface Event {
-  title: string
-  startDate: Date 
-  description: string
-  registrations: [UserRegistration]
-}
